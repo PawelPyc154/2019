72b9845277cd58ecd6d4569835316550bd38a12b
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-01-20 13:26:43 +0100

    add section CONTACT DETAILS

---
+++ src/pages/contact/sections/Details.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { style1Type, style1 } from '../../../state/style/style1';
+
+export interface DetailsProps {}
+
+const Details: React.SFC<DetailsProps> = () => {
+  return (
+    <Section styled={style1}>
+      <h3>CONTACT DETAILS</h3>
+      <span>ADDRESS</span>
+      <p>355 E STH ST, MANHATTAN, NY 10003</p>
+      <span>PHONE</span>
+      <p>+939 777 55 33</p>
+      <span>EMAIL</span>
+      <p>RESERVATION@RENAME.COM</p>
+    </Section>
+  );
+};
+
+export default Details;
+
+const Section = styled.section<style1Type>`
+  padding: 5px;
+
+  & > h3 {
+    font-size: 2em;
+    color: ${props => props.styled.spanColor};
+    margin-bottom: 20px;
+  }
+  & > span {
+    color: ${props => props.styled.spanColor};
+    font-size: 1.5em;
+    margin-left: 15px;
+  }
+  & > p {
+    font-size: 1.2em;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+`;
