b1892e22b1a1db53388089c45eb7ab4579f625b6
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-01-29 13:22:23 +0100

    clean code console log

--- src/pages/shop/components/ShopContainerItems.tsx
+++ src/pages/shop/components/ShopContainerItems.tsx
@@ -39,25 +39,20 @@ const ShopContainerItems: React.SFC<ShopContainerItemsProps> = ({ activePage }) 
     const type = location.pathname.slice(6);
     if (type !== 'allproducts') {
       setMealsProps(meals.filter(item => item.type === type));
-      // setPages(Math.ceil(mealsProps.length / 6));
-      // const mealsPropsCoppy = [...mealsProps];
-      // setmealss(mealsPropsCoppy.splice((active - 1) * 6, 6));
+
     } else {
       setMealsProps(meals);
-      // setPages(Math.ceil(mealsProps.length / 6));
-      // const mealsPropsCoppy = [...mealsProps];
-      // setmealss(mealsPropsCoppy.splice((active - 1) * 6, 6));
+  
     }
   }, [location]);
 
-  // useEffect(() => {}, [mealsProps]);
 
   useEffect(() => {
     setPages(Math.ceil(mealsProps.length / 6));
     const mealsPropsCoppy = [...mealsProps];
     setmealss(mealsPropsCoppy.splice((active - 1) * 6, 6));
   }, [active, mealsProps]);
-  // console.log(mealss);
+
 
   return (
     <>
