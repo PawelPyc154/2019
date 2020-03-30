06c1ca4b7b10238d804411b075b4c95fe6a6d3d0
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-03-30 15:35:39 +0200

    useScroll rename variable

--- src/middleware/useScroll.ts
+++ src/middleware/useScroll.ts
@@ -1,20 +1,20 @@
 import { useState, useEffect } from 'react';
 
-const useScroll = (tagRef: React.RefObject<HTMLDivElement>, yOffset: number = 1): boolean => {
+const useScroll = (ref: React.RefObject<HTMLDivElement>, yOffset: number = 1): boolean => {
   const [scrollY, setScrollY] = useState(0);
   const [animationStart, setAnimationStart] = useState(false);
 
   useEffect(() => {
     if (scrollY > -yOffset) {
       window.addEventListener('scroll', () => {
-        if (tagRef.current) {
-          setScrollY(tagRef.current.getBoundingClientRect().y - window.innerHeight);
+        if (ref.current) {
+          setScrollY(ref.current.getBoundingClientRect().y - window.innerHeight);
         }
       });
     } else {
       setAnimationStart(true);
     }
-  }, [scrollY, tagRef, yOffset]);
+  }, [scrollY, ref, yOffset]);
 
   return animationStart;
 };
